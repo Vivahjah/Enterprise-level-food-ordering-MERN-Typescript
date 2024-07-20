96c8566b6d76287087d807bfc026b8c05cd3ccf7
Mengwa Ugo <mengwa44@yahoo.com>
2024-07-20 04:48:10 +0100

    conneted to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,38 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
-import "dotenv/config"
+import "dotenv/config";
+import mongoose from "mongoose";
+import connectDB from "./DB/connect";
 
+const MONGO_URI = "mongodb+srv://vivahjah:jJwuWrNmdS8Db8f2@enterprise-food-orderin.xlvlqva.mongodb.net/?retryWrites=true&w=majority&appName=enterprise-food-ordering-app"
+mongoose.connect(MONGO_URI).then(()=> console.log("Connected to db")).catch((error) => console.log(error))
 
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello Word!" })
-})
+    res.json({ message: "Hello World!" });
+});
+
+const startServer = async() => {
+    try {
+        await connectDB()
+        app.listen(7000, () =>
+            console.log("Connected to DB!!")
+        );
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
 
 
-app.listen(7000, () => {
-    console.log("server started on localhost:7000");
-})
+
+
+
+
+
+
