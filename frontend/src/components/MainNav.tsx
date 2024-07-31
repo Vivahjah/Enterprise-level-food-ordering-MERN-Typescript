f0d10d66358da70026627a1bb85e0b17a972aa5a
Mengwa Ugo <mengwa44@yahoo.com>
2024-07-31 21:09:33 +0100

    completed lnding page

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button"
+
+
+const MainNav = () => {
+  return (
+   <Button variant="ghost" className="text-white bg-orange-500 font-bold hover:bg-orange-600 hover:text-white">Log In</Button>
+  )
+}
+
+export default MainNav
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+
+import appDownload from "../assets/appDownload.png"
+import landigImage from "../assets/landing.png"
+const HomePage = () => {
+  return (
+    <div className="flex flex-col gap-12">
+        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 -mt-16 text-center">
+            <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                Tuck into a takeway today
+            </h1>
+            <span className="text-xl">Food is just a click away!</span>
+        </div>
+        <div className="grid md:grid-cols-2 gap-5">
+            <img src={landigImage} />
+            <div className="flex flex-col items-center justify-center gap-4 text-center">
+                <span className="font-bold text-3xl tracking-tighter">
+                    Order takeaway even faster!
+                </span>
+                <span>Download the MernEats App for faster ordeing and personalised recommendations</span>
+                <img src={appDownload} />
+            </div>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default HomePage
